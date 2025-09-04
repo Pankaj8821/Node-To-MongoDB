d53614c0036d4353aa318541b1617847e738a2e2
Pankaj Kumar Yadav <131384457+Pankaj8821@users.noreply.github.com>
2025-09-04 22:03:03 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,11 +3,8 @@ const { MongoClient } = require('mongodb');
 const username = process.env.MONGO_USERNAME;
 const password = process.env.MONGO_PASSWORD;
 const host = process.env.MONGO_HOST;
-
 const port = process.env.MONGO_PORT || 27017;
-
 const uri = `mongodb://${username}:${password}@${host}:${port}/?authSource=admin`;
-
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
