cd7b8554d31641141b3c18f0d2ff0577084903db
Pankaj Kumar Yadav <131384457+Pankaj8821@users.noreply.github.com>
2025-09-05 13:11:51 +0530

    Delete k8s/helm-prometheus.tf

--- k8s/helm-prometheus.tf
+++
@@ -1,9 +0,0 @@
-resource "helm_release" "prometheus" {
-  name             = "prometheus"
-  repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "kube-prometheus-stack"
-  namespace        = "monitoring"
-  create_namespace = true
-
-  values = [file("${path.module}/values-prometheus.yaml")]
-}
