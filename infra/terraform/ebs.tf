287283e93223fcb782d7bef902b015d5eee41f3a
Pankaj Kumar Yadav <131384457+Pankaj8821@users.noreply.github.com>
2025-09-05 17:07:47 +0530

    Update ebs.tf

--- infra/terraform/ebs.tf
+++ infra/terraform/ebs.tf
@@ -1,1 +1,24 @@
+# IAM Role for EBS CSI Driver
+resource "aws_iam_role" "ebs_csi_driver" {
+  name = "AmazonEKS_EBS_CSI_DriverRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.oidc.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(aws_eks_cluster.eks.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          }
+        }
+      }
+    ]
+  })
+}
+
 
